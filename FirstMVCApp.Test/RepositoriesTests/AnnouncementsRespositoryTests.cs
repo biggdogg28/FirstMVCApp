ab5569eb1d60e1a322fea351b35a358988b7001f
Lehel Veibel <lehel.veibel@umtsoftware.com>
2023-03-08 19:27:52 +0200

    Added Helper folder with DBContext for Unit Tests

--- FirstMVCApp.Test/RepositoriesTests/AnnouncementsRespositoryTests.cs
+++ FirstMVCApp.Test/RepositoriesTests/AnnouncementsRespositoryTests.cs
@@ -1,53 +1,88 @@
-﻿using FirstMVCApp.Models;
+﻿using FirstMVCApp.DataContext;
+using FirstMVCApp.Models;
+using FirstMVCApp.Repositories;
 using System;
 using System.Collections.Generic;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FirstMVCApp.Test.Helpers;
 
 namespace FirstMVCApp.Test.RepositoriesTests
 {
     [TestClass]
-    internal class AnnouncementsRespositoryTests
+    public class AnnouncementsRespositoryTests
     {
+        private readonly AnnouncementsRepository _repository;
+        private readonly ProgrammingClubDataContext _contextInMemory;
+
+        public AnnouncementsRespositoryTests()
+        {
+            _contextInMemory = Helpers.DBContextHelper.GetDatabaseContext();
+            _repository = new AnnouncementsRepository(_contextInMemory);
+        }
+
+
         [TestMethod]
         public void GetAllAnnouncements()
         {
+
             // Arrange
-            AnnouncementModel announcement1 = new AnnouncementModel
+            AnnouncementModel announcement1 = new AnnouncementModel()
             {
                 IDAnnouncement = Guid.NewGuid(),
-                ValidFrom = DateTime.Now,
-                ValidTo = DateTime.Now,
-                EventDate = DateTime.Now,
+                ValidFrom = new DateTime(2023, 05, 02),
+                ValidTo = new DateTime(2023, 05, 02),
+                EventDate = new DateTime(2023, 05, 02),
                 Tags = "Tags1",
                 Text = "Announcement",
                 Title = "Event1",
             };
 
-            AnnouncementModel announcement2 = new AnnouncementModel
+            AnnouncementModel announcement3 = new AnnouncementModel()
+            {
+                EventDate = new DateTime(2022, 05, 02),
+                IDAnnouncement = Guid.NewGuid(),
+                ValidFrom = new DateTime(2022, 08, 06),
+                ValidTo = new DateTime(2021, 06, 5),
+                Tags = "Tag",
+                Title = "title",
+                Text = "Dexter"
+            };
+
+            AnnouncementModel announcement2 = new AnnouncementModel()
             {
                 IDAnnouncement = Guid.NewGuid(),
-                ValidFrom = DateTime.Now,
-                ValidTo = DateTime.Now,
-                EventDate = DateTime.Now,
-                Tags = "Tags1",
-                Text = "Announcement",
-                Title = "Event1",
+                ValidFrom = new DateTime(2023, 05, 02),
+                ValidTo = new DateTime(2023, 05, 02),
+                EventDate = new DateTime(2023, 05, 02),
+                Tags = "Tags2",
+                Text = "Announcement2",
+                Title = "Event2",
             };
+
+            List<AnnouncementModel> newList = new List<AnnouncementModel>();
+            newList.Add(announcement3);
 
             List<AnnouncementModel> list = new List<AnnouncementModel>();
             list.Add(announcement1);
             list.Add(announcement2);
+            list.Add(announcement3);
+            Helpers.DBContextHelper.AddAnnouncement(_contextInMemory, announcement1);
+            Helpers.DBContextHelper.AddAnnouncement(_contextInMemory, announcement2);
+            Helpers.DBContextHelper.AddAnnouncement(_contextInMemory, announcement3);
+
 
             // Act 
-            
+            List<AnnouncementModel> dbAnnouncements = _repository.GetAnnouncements().ToList();
 
 
             // Assert
 
-            Assert.AreEqual(1, list.Count);
-            Assert.AreEqual("Event1", list.First().Tags);
+            Assert.AreEqual(list.Count, dbAnnouncements.Count);
+
+            // Assert.AreEqual("Event1", list.First().Tags);
         }
     }
 }
